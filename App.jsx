9a11cfb1a56b5332270e6df5a17450c8b9a28a43
pranjal <you@example.com>
2025-11-03 22:39:35 +0530

    Added App.jsx component

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Nebula Notes 🌌</h1>
+      <p>This is your first reactive notes app using Vite + React!</p>
+    </div>
+  );
+}
+
+export default App;
